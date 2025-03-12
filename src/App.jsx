3014962b0bd4a78e9abc38a45ad92276956f6c63
Diego Manildo <diegothiry26@gmail.com>
2025-03-12 11:36:54 -0300

    Implemented localStorage save

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,15 @@ import { BoardModal } from "./components/BoardModal";
 import TurnModal from "./components/TurnModal";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X;
+  });
   
   const [winner, setWinner] = useState(null);
 
@@ -18,6 +24,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   }
 
   const updateBoard = (index) => {
@@ -30,6 +39,9 @@ function App() {
     
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    window.localStorage.setItem("board", JSON.stringify(newBoard))
+    window.localStorage.setItem("turn", JSON.stringify(newTurn))
 
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
